7d0646492a4b1de1f793a280b8097141d54c08a6
Joshua Bradford <jjyet@yahoo.com>
2017-05-01 12:28:00 -0400

    added puppy and fun message

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src="http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-21.jpg" className="App-logo" alt="logo" />
+          <h2>Welcome to React (puppy version)</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is where we have fun and change stuff and make cool computer things happen.
         </p>
       </div>
     );
